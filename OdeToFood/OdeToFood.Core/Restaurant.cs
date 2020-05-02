fabfb3d15d3158c97b7bc71c16ead09e0e418c6d
Marlon <marlonatton@gmail.com>
2020-05-03 00:23:32 +1000

    Added basic file Structure to handle data flow between web application and database

--- OdeToFood/OdeToFood.Core/Restaurant.cs
+++ OdeToFood/OdeToFood.Core/Restaurant.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace OdeToFood.Core
 {
+
     public class Restaurant
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public CuisineType Cuisine { get; set; }
     }
 }
