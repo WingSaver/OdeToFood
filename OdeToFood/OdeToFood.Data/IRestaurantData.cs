ba90b87b196658724805749d8bd90f28e60efae8
Marlon <marlonatton@gmail.com>
2020-05-03 01:32:15 +1000

    Add Data layer project, also updated UI using rage page to display a table generated based of the data returned from Entity

---
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,45 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{Id=1, Name="Pizz Hut", Cuisine=CuisineType.Italian, Location="Burwood"},
+                new Restaurant{Id=2, Name="China Bar", Cuisine=CuisineType.None, Location="Box Hill"},
+                new Restaurant{Id=3, Name="TarcoBell", Cuisine=CuisineType.Mexican, Location="Ringwood"},
+                new Restaurant{Id=4, Name="Kazaan", Cuisine=CuisineType.Indian, Location="Knox"}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            //return from r in restaurants
+            //       orderby r.
+            var result = restaurants.OrderBy(or => or.Name);
+
+            var result1 = from r in restaurants
+                          orderby r.Name
+                          select r;
+
+            //return (from r in restaurants
+            //        orderby r.Name
+            //        select r);
+
+            return result;
+        }
+    }
+}
